1d33f9311675f7f063307d8f06bea47d15b9c0a1
Alex Babou <ababou@dendev.net>
2021-05-15 19:41:03 -0600

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Alex Babou on 5/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
